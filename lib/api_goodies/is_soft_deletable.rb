50e2a6ce1879e2a91f0e22c3d292ad3c1ebbaa23
Andrew Ryan Lazarus <nerdrew@gmail.com>
2014-02-05 10:56:29 -0800

    Add callbacks to soft_delete

--- lib/api_goodies/is_soft_deletable.rb
+++ lib/api_goodies/is_soft_deletable.rb
@@ -1,9 +1,13 @@
 module APIGoodies
   module IsSoftDeletable
     def is_soft_deletable
+      # TODO have separate modules for rails3 vs rails4
       self.send :include, InstanceMethods
+      self.send :extend, ClassMethods
 
       scope :active, ->{ where(deleted: false) }
+
+      define_model_callbacks :soft_delete
 
       begin
         attr_protected :deleted, :deleted_at
@@ -12,10 +16,17 @@ module APIGoodies
       end
     end
 
+    module ClassMethods
+      def soft_delete
+        update_all(deleted: true, deleted_at: Time.now)
+      end
+    end
+
     module InstanceMethods
       def soft_delete
-        update_column(:deleted, true)
-        update_column(:deleted_at, Time.now)
+        run_callbacks :soft_delete do
+          update_columns(deleted: true, deleted_at: Time.now)
+        end
       end
     end
   end
--- lib/api_goodies/rspec/shared_examples/is_soft_deletable.rb
+++ lib/api_goodies/rspec/shared_examples/is_soft_deletable.rb
@@ -18,6 +18,71 @@ shared_examples 'api_goodies gem is_soft_deletable' do
     end
   end
 
+  describe ".soft_delete" do
+    it "sets the 'deleted' attribute to true for all of the models" do
+      model1 = build_valid_model
+      model1.deleted = false
+      model1.save!
+
+      model2 = build_valid_model
+      model2.deleted = false
+      model2.save!
+
+      described_class.active.should == [model1, model2]
+
+      described_class.active.soft_delete
+
+      described_class.active.should be_empty
+    end
+  end
+
+  describe 'callbacks' do
+    it "has a before_soft_delete callback" do
+      model = create_valid_model
+
+      model.class.module_eval do
+        attr_accessor :foo
+        before_soft_delete ->(mod){ mod.foo = 'bam' }
+      end
+
+      model.foo = 'boom'
+      model.soft_delete
+      model.foo.should == 'bam'
+    end
+
+    it "has an after_soft_delete callback" do
+      model = create_valid_model
+
+      model.class.module_eval do
+        attr_accessor :foo
+        after_soft_delete ->(mod){ mod.foo = 'bam' }
+      end
+
+      model.foo = 'boom'
+      model.soft_delete
+      model.foo.should == 'bam'
+    end
+
+    it "has an around_soft_delete callback" do
+      model = create_valid_model
+
+      model.class.module_eval do
+        attr_accessor :foo
+        around_soft_delete :test_around_callback
+
+        def test_around_callback
+          self.foo = 'bam'
+          yield
+        end
+      end
+
+      model.foo = 'boom'
+      model.soft_delete
+      model.foo.should == 'bam'
+      model.should be_deleted
+    end
+  end
+
   describe "#soft_delete" do
     it "sets the 'deleted' attribute to true" do
       model = build_valid_model
